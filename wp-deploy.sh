9b7bc08e7f556b2e01b096e0cf4960ca5cc6681d
root <root@ip-172-31-17-201.ap-south-1.compute.internal>
2018-12-20 11:16:04 +0000

    updated build code

--- wp-deploy.sh
+++ wp-deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
-yum update -y 
-yum install httpd -y 
-echo "<html><center>This is deployment---1</center></html>" > /var/www/html/index.html
-service httpd start
-chkconfig httpd on 
+sudo yum update -y 
+sudo yum install httpd -y 
+sudo service httpd start
+sudo chkconfig httpd on 
